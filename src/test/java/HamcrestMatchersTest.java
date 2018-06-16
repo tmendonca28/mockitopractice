275f68569753837059b8be0ad9a4f8c70ddb888c
Anthony Mendonca <anthonymendonca@Anthonys-MacBook-Pro.local>
2018-06-16 10:39:03 +0300

    Intro to hamcrest matchers and assertThat

--- src/test/java/HamcrestMatchersTest.java
+++ src/test/java/HamcrestMatchersTest.java
@@ -1,13 +1,33 @@
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers.*;
 
 public class HamcrestMatchersTest {
     @Test
     public void test() {
         List<Integer> scores = Arrays.asList(99, 100, 105, 101);
         // I wanna first assert that scores has 4 items
+        assertThat(scores, hasSize(4));
+        assertThat(scores, hasItems(99, 100));
 
+        // Check that every items in the list passes some condition e.g >90
+        assertThat(scores, everyItem(greaterThan(90)));
+        assertThat(scores, everyItem(lessThan(190)));
+
+        // String-related assertions
+        assertThat("", isEmptyString());
+        assertThat(null, isEmptyOrNullString());
+
+        // Arrays
+        Integer[] marks = {1,2,3};
+        assertThat(marks, Matchers.<Integer>arrayWithSize(3));
+        assertThat(marks, arrayContaining(1,2,3));
+        assertThat(marks, arrayContainingInAnyOrder(2,1,3));
     }
 }
